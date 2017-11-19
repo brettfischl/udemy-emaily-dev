f438a86acbe0510ae39e831811808ad86d991e97
brettfischl <brettfischl1@gmail.com>
2017-11-19 16:01:26 -0500

    lol

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google.callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-app.get('/hey-sophie', (req, res) => {
-  res.send('brett gr8');
-});
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
+// app.get(
+//   '/auth/google/callback',
+//   req, res
+// )
+
+app.get('/hello-chloe', function (req, res) {
+  res.send('BRETT GR8')
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
